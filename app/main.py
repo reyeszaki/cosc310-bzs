24ea03b61ce3cc8d7819bc757a6e39a6a285c4b9
sgoh01 <shawnjgoh776@gmail.com>
2026-09-25 11:40:21 -0700

    Updated 4 layer backend

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
-from app.routers import restaurants
+
+from app.api.routers import restaurant_routers
 
 app = FastAPI(title="COSC 310 Food Delivery API")
 
-app.include_router(restaurants.router)
+app.include_router(restaurant_routers.router)
 
 
 @app.get("/health")
